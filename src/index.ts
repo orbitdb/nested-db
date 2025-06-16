4872291ae8a59245008347d05992b3272d048269
julienmalard <julien.malard@mail.mcgill.ca>
2025-06-16 14:40:03 +0530

    Export types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export { default as Nested, NestedDatabaseType } from "@/nested.js";
 
-export { registerNested, flatten, toNested } from "@/utils.js";
+export { registerNested, flatten, toNested, splitKey, joinKey, isSubkey } from "@/utils.js";
+
+export { NestedKey, PossiblyNestedValue, NestedValue } from "@/types.js";
 
 export { version } from "@/version.js";
